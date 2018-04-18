0d77cb600ba97ed6848e52d588f0727672478b14
foicoin <36694651+foicoin@users.noreply.github.com>
2018-04-18 16:37:48 +0900

    Update mist.js

--- app/client/mist.js
+++ app/client/mist.js
@@ -45,7 +45,7 @@ updateMistMenu = function(){
             mist.menu.add(account._id,{
                 position: 3 + index,
                 name: account.name,
-                badge: EthTools.formatBalance(account.balance, "0 a", 'ether')+ ' ETH',
+                badge: EthTools.formatBalance(account.balance, "0 a", 'foi')+ ' FOI',
                 selected: (location.pathname === '/account/'+ account.address)
             }, function(){
                 FlowRouter.go('/account/'+ account.address);
